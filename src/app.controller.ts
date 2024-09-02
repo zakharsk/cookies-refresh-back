f1a47660a9be0eb1d37b9c618ee1704818f57cb8
Zakhar Skorokhodov <zaharsk@gmail.com>
2024-09-02 09:55:42 +0200

    chore(): Added health check route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { NoAccessTokenCookie } from './passport/decorators';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,16 @@ export class AppController {
   @Get()
   main() {
     return this.appService.main();
+  }
+
+  @NoAccessTokenCookie()
+  @Get('healthz')
+  healthCheck() {
+    return {
+      name: process.env.npm_package_name,
+      version: process.env.npm_package_version,
+      description: process.env.npm_package_description,
+      homepage: process.env.npm_package_homepage,
+    };
   }
 }
