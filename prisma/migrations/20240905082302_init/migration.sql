cf846fea2114ac13ec27babb4c693c1213ca4478
Zakhar Skorokhodov <zaharsk@gmail.com>
2024-09-05 10:26:22 +0200

    chore(): Change DB from SQLite to Postgres

--- prisma/migrations/20240905082302_init/migration.sql
+++ prisma/migrations/20240905082302_init/migration.sql
@@ -1,11 +1,13 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "login" TEXT NOT NULL,
     "passwordHash" TEXT NOT NULL,
     "refreshTokenHash" TEXT,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
